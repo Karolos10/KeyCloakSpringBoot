23569132dc23c8a0a45bcd4948e811c8ccd8efc3
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-02 22:10:08 -0500

    Added KeycloacServiceImpl to implement IKeycloakService for managing Keycloak users, including listing all users, searching users by username, and creating new users.

--- src/main/java/com/api/rest/controller/dato/UserDTO.java
+++ src/main/java/com/api/rest/controller/dato/UserDTO.java
@@ -6,8 +6,17 @@ import lombok.Value;
 
 import java.util.Set;
 
+@Value
+@RequiredArgsConstructor
 @Builder
-public record UserDTO(String username, String email, String firstName, String lastName, String password,
-                      Set<String> roles) {
+public class UserDTO {
+
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private Set<String> roles;
+
 
 }
---
+++ src/main/java/com/api/rest/service/impl/KeycloacServiceImpl.java
@@ -0,0 +1,118 @@
+package com.api.rest.service.impl;
+
+import com.api.rest.controller.dato.UserDTO;
+import com.api.rest.service.IKeycloakService;
+import com.api.rest.util.KeycloackProvider;
+import jakarta.ws.rs.core.Response;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+
+public class KeycloacServiceImpl implements IKeycloakService {
+
+    /**
+     * Metodo para listar todos los ususarios de Keycloack
+     *
+     * @return List<UserRepresentation>
+     */
+    @Override
+    public List<UserRepresentation> findAllUsers() {
+        return KeycloackProvider.getRealmResource()
+                .users()
+                .list();
+    }
+
+    /**
+     * Metodo para buscar un usuario por su username
+     *
+     * @return List<UserRepresentation>
+     */
+    @Override
+    public List<UserRepresentation> searchUserByUsername(String username) {
+        return KeycloackProvider.getRealmResource()
+                .users()
+                .searchByUsername(username, true);
+    }
+
+    /**
+     * Metodo para crear un usuario en Keycloack
+     * @return String
+     */
+    @Override
+    public String createUser(@NonNull UserDTO userDTO) {
+
+        int status = 0;
+        UsersResource userResource = KeycloackProvider.getUserResource();
+        UserRepresentation userRepresentation = new UserRepresentation();
+
+        userRepresentation.setFirstName(userDTO.getFirstName());
+        userRepresentation.setLastName(userDTO.getLastName());
+        userRepresentation.setEmail(userDTO.getEmail());
+        userRepresentation.setUsername(userDTO.getUsername());
+        userRepresentation.setEmailVerified(true);
+        userRepresentation.setEnabled(true);
+
+        Response response = userResource.create(userRepresentation);
+        status = response.getStatus();
+
+        if(status == 201){
+            String path = response.getLocation().getPath();
+            String userId = path.substring(path.lastIndexOf("/") + 1);
+
+            CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+            credentialRepresentation.setTemporary(false);
+            credentialRepresentation.setType(OAuth2Constants.PASSWORD);
+            credentialRepresentation.setValue(userDTO.getPassword());
+
+            userResource.get(userId)
+                    .resetPassword(credentialRepresentation);
+
+            RealmResource realmResource = KeycloackProvider.getRealmResource();
+
+            List<RoleRepresentation> roleRepresentations = null;
+
+            if(userDTO.getRoles() == null || userDTO.getRoles().isEmpty()){
+                roleRepresentations = List.of(realmResource.roles().get("user").toRepresentation());
+            }else{
+                roleRepresentations = realmResource.roles()
+                        .list()
+                        .stream()
+                        .filter(role -> userDTO.getRoles()
+                                .stream()
+                                .anyMatch(roleName -> roleName.equalsIgnoreCase(role.getName())))
+                        .toList();
+            }
+
+            realmResource.users()
+                    .get(userId)
+                    .roles()
+                    .realmLevel()
+                    .add(roleRepresentations);
+
+            return "User created successfully";
+        }else if (status == 409) {
+            /*log.error("User already exists");*/
+            return "User already exists";
+        }else {
+            /*log.error("Error creating user");*/
+            return "Error creating user";
+        }
+
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public void updateUser(String userId, UserDTO userDTO) {
+
+    }
+}
