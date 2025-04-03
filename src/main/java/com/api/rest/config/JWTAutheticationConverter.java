95fb519e07be6a733232ea9f4aba5c2d43ede199
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-02 19:15:35 -0500

    Fixed invalid JWK Set URL in security configuration - Corrected the JWK Set URL in pplication.properties or pplication.yml to remove extra double quotes. Uncommented the @Component annotation in JWTAutheticationConverter.java to enable the JWT authentication converter.

---
+++ src/main/java/com/api/rest/config/JWTAutheticationConverter.java
@@ -0,0 +1,10 @@
+package com.api.rest.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+/*@Component*/
+/*public class JWTAutheticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+}*/
